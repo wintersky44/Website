04757c30205476e48a073a4765f24dfd3a3c7861
wintersky <idk316675@gmail.com>
2026-05-29 20:27:49 +0530

    .

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("Hi, i’m V")
+st.title("Hi, i'm V")
 
-st.markdown("""
-            
-### about me
+st.subheader("about me",divider = "gray")
+
+st.markdown("""           
 
 i’m a 22 year old from New Castle, DE. My interests include data science, ML, philosophy,
 movies and tv shows, social media, and everything else.
@@ -22,3 +22,34 @@ i believe in the hard problem of consciousness, i.e, the question of how a first
 experience can arise from physical matter.
 
 """)
+
+st.subheader("my socials",divider = "gray")
+
+# st.image("https://images.icon-icons.com/3685/PNG/512/github_logo_icon_229278.png", width = 30)
+# st.link_button("GitHub",'https://github.com/wintersky44')
+
+# st.image("https://images.squarespace-cdn.com/content/v1/54a5505fe4b0d132f64e0e9b/1591957981477-VQJU5KFVSWHKCK5NIBLJ/Pinterest-Logo-1x1.png", width = 30)
+# st.link_button("Pinterest","https://pin.it/4TyXAUw84")
+
+col1, col2, col3, col4, col5, col6 = st.columns(6) 
+
+with col1:
+    st.image("https://images.icon-icons.com/3685/PNG/512/github_logo_icon_229278.png", width = 30)
+    st.link_button("GitHub",'https://github.com/wintersky44')
+with col2:
+    st.image("https://images.squarespace-cdn.com/content/v1/54a5505fe4b0d132f64e0e9b/1591957981477-VQJU5KFVSWHKCK5NIBLJ/Pinterest-Logo-1x1.png", width = 30)
+    st.link_button("Pinterest","https://pin.it/4TyXAUw84")
+
+with col3:
+    st.image("https://a.ltrbxd.com/logos/letterboxd-decal-dots-pos-rgb-500px.png", width = 30)
+    st.link_button("Letterboxd", "https://boxd.it/7ZQuN")
+
+with col4:
+    st.image("https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Reddit_Logo_Icon.svg/250px-Reddit_Logo_Icon.svg.png", width = 30)
+    st.link_button("Reddit", "https://www.reddit.com/user/cftvkjhbkf/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button")
+
+with col5:
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Youtube_Music_icon.svg/3840px-Youtube_Music_icon.svg.png", width = 30)
+    st.link_button("YT music", "https://music.youtube.com/@abc123-e1h?si=kfyjkz34sLDTsyqg") 
+
+
