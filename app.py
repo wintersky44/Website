153d3f7081efc9726338aacfc9b2281699790bd4
wintersky <idk316675@gmail.com>
2026-05-29 16:06:27 +0530

    .

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.markdown("""
+
+# Hi, i’m V
+            
+### about me
+
+i’m a 22 year old from New Castle, DE. My interests include data science, ML, philosophy,
+movies and tv shows, social media, and everything else.
+
+i’m currently working on data science projects and i like turning my 
+models and data analysis into web apps and dashboards using streamlit.
+
+i’m interested in the discussions surrounding free will and moral responsibility,
+i mostly lean towards the impossibility of basic desert moral responsibility, 
+supported by Galen Strawson’s basic argument. i also like Derk Perebooms hard incompatibilist
+position, especially his manipulation arguments.
+
+i’m an anti realist on all sorts of things, including personal identity, gender, morality etc. 
+i believe in the hard problem of consciousness, i.e, the question of how a first person, subjective 
+experience can arise from physical matter.
+
+""")
